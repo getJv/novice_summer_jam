64c1ac602502bc7a92144ae8b684226d03f24004
Jhonatan Morais <jhonatanvinicius@gmail.com>
2025-06-28 07:46:46 -0300

    feat: Adjust the code to override the main functions by scenes

---
+++ main_scene.lua
@@ -0,0 +1,12 @@
+
+function main_scene_init()
+    _update = main_scene_update
+    _draw = main_scene_draw
+end
+function main_scene_update()
+
+end
+function main_scene_draw()
+    cls()
+    print("Hello world with split files")
+end
---
+++ pre_loader.lua
@@ -0,0 +1,1 @@
+_init = main_scene_init
